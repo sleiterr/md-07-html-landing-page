e9c59612dee3e1e90125fcd19b7be7b0fb4aa78b
Oleg Troian <sleite4r@gmail.com>
2024-09-04 20:43:57 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,31 @@
 // JS
 
-const hamburger = document.getElementById("burger");
+   const hamburger = document.querySelector(".burger-btn");
+   const navUL = document.querySelector(".nav-list");
+   const bars = document.querySelectorAll(".bar");
 
-const havUl = document.getElementById("navUL");
+   hamburger.addEventListener("click", () => {
+     navUL.classList.toggle("show");
+     bars.forEach((element) => {
+       element.classList.toggle("active");
+     });
+   });
 
-hamburger.addEventListener("click", () => {
-  navUL.classList.toggle("show");
-  hamburger.classList.toggle("active");
-});
+// const hamburger = document.getElementById("burger");
 
-navUL.addEventListener("click", () => {
-  navUL.classList.remove("show");
-  hamburger.classList.remove("active");
-});
+// const havUl = document.getElementById("navUL");
 
-navUL.addEventListener("click", () => {
-  navUL.classList.remove("show");
-  hamburger.classList.remove("active");
-});
+// hamburger.addEventListener("click", () => {
+//   navUL.classList.toggle("show");
+//   hamburger.classList.toggle("active");
+// });
+
+// navUL.addEventListener("click", () => {
+//   navUL.classList.remove("show");
+//   hamburger.classList.remove("active");
+// });
+
+// navUL.addEventListener("click", () => {
+//   navUL.classList.remove("show");
+//   hamburger.classList.remove("active");
+// });
